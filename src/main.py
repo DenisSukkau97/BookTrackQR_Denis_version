a64ed9cb7e7f0cdee62cfe33647a9008e21850c9
student <sakir.demiral@sbs-herzogenaurach.de>
2026-02-24 08:37:59 +0100

    Anschaulicher gemacht

--- src/main.py
+++ src/main.py
@@ -1,15 +1,9 @@
 import sys
-
 from PyQt6.QtWidgets import QApplication
-
 from MainWindow import MainWindow
 
 if __name__ == '__main__':
-    # All you need is
-    # https://doc.qt.io/qtforpython/
     app = QApplication(sys.argv)
-
     main_window = MainWindow()
     main_window.show()
-
     sys.exit(app.exec())
